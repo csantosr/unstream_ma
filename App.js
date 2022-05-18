3cb5b8b76b9ee25c657316458984bd7e7269b073
Cristian Fernando Santos Ribero <csantosr@Cristians-MacBook-Pro.local>
2022-05-18 07:57:27 -0500

    Integrating Apollo

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React, {useEffect} from 'react';
@@ -5,21 +6,28 @@ import SplashScreen from 'react-native-splash-screen'
 import LoginScreen from './src/containers/Login';
 import StreamScreen from './src/containers/Stream';
 
+const Stack = createNativeStackNavigator();
+
+const client = new ApolloClient({
+  uri: 'localhost:5000/graphql',
+  cache: new InMemoryCache()
+});
+
 const App = () => {
 
   useEffect(() => {
     SplashScreen.hide();
   }, []);
 
-  const Stack = createNativeStackNavigator();
-
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Stream" component={StreamScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Stream" component={StreamScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ApolloProvider>
   );
 };
 
