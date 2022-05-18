919ff1604cc93079a49b2c6137764a5eb2886d5c
Cristian Fernando Santos Ribero <csantosr@Cristians-MacBook-Pro.local>
2022-05-18 07:54:53 -0500

    Routing

--- App.js
+++ App.js
@@ -1,31 +1,25 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React, {useEffect} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  Text,
-  View,
-} from 'react-native';
 import SplashScreen from 'react-native-splash-screen'
+import LoginScreen from './src/containers/Login';
+import StreamScreen from './src/containers/Stream';
 
 const App = () => {
 
   useEffect(() => {
     SplashScreen.hide();
-  }, [])
-  
+  }, []);
+
+  const Stack = createNativeStackNavigator();
 
   return (
-    <SafeAreaView>
-      <StatusBar barStyle="light-content" />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-      >
-        <View>
-          <Text>unstream</Text>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Stream" component={StreamScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
